060f7c5aa07c5b80191e6d99a2e3b7cdddb4dd99
anandrahul007 <rahulanandmtech@gmail.com>
2022-09-07 11:37:05 +0530

    updatd

---
+++ updated.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=
+    
+    , initial-scale=1.0">
+    <title>git hub</title>
+</head>
+<body>
+
+<p>
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet eaque quis laudantium accusamus molestias magnam eligendi ratione, sapiente iste commodi.
+</p>
+    
+</body>
+</html>
